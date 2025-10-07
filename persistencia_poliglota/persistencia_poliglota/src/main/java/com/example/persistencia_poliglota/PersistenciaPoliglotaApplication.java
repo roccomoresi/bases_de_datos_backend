87346e2a9b8670b8be55b2814d8d60b4db5a0e78
Rocco Moresi <137230323+roccomoresi@users.noreply.github.com>
2025-10-07 08:27:12 -0300

    first commit

---
+++ persistencia_poliglota/persistencia_poliglota/src/main/java/com/example/persistencia_poliglota/PersistenciaPoliglotaApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistencia_poliglota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaPoliglotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaPoliglotaApplication.class, args);
+	}
+
+}
---
+++ persistencia_poliglota/persistencia_poliglota/src/test/java/com/example/persistencia_poliglota/PersistenciaPoliglotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persistencia_poliglota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaPoliglotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
