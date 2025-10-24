5a19820aee6053d5a9697cebfb6321a7919a4d29
Rocco Moresi <137230323+roccomoresi@users.noreply.github.com>
2025-10-24 19:15:25 -0300

    filtro fix

--- src/main/java/com/example/persistencia/poliglota/repository/cassandra/MedicionRepository.java
+++ src/main/java/com/example/persistencia/poliglota/repository/cassandra/MedicionRepository.java
@@ -1,7 +1,10 @@
 package com.example.persistencia.poliglota.repository.cassandra;
 
 import com.example.persistencia.poliglota.model.cassandra.Medicion;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -13,6 +16,7 @@ public interface MedicionRepository extends CassandraRepository<Medicion, UUID> 
 
     List<Medicion> findBySensorId(UUID sensorId);
 
+    @Query("SELECT * FROM mediciones_por_sensor WHERE sensor_id = ?0 AND fecha_medicion >= ?1 AND fecha_medicion <= ?2")
     List<Medicion> findBySensorIdAndFechaMedicionBetween(UUID sensorId, Date inicio, Date fin);
 
 }
