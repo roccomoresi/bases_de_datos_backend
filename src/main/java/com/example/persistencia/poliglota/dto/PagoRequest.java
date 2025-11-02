4e7c7266d79e37f60491bf7e965c9fda0db73224
Candelaria Arbe <candelariaarbe@gmail.com>
2025-11-02 20:50:39 -0300

    Update PagoRequest.java

--- src/main/java/com/example/persistencia/poliglota/dto/PagoRequest.java
+++ src/main/java/com/example/persistencia/poliglota/dto/PagoRequest.java
@@ -3,7 +3,15 @@ package com.example.persistencia.poliglota.dto;
 public class PagoRequest {
     private String metodoPago;
     private Double monto;
+    private Integer idFactura;
 
+
+    public Integer getIdFactura() {
+        return idFactura;
+    }
+    public void setIdFactura(Integer idFactura) {
+        this.idFactura = idFactura;
+    }
     public String getMetodoPago() { return metodoPago; }
     public void setMetodoPago(String metodoPago) { this.metodoPago = metodoPago; }
     public Double getMonto() { return monto; }
