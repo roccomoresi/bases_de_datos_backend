27bdc26e599ca4abaaefb8b7cab866d962b42549
CandeFernandez1 <cfernandez1@uade.edu.ar>
2025-11-03 23:44:20 -0300

    Update Corsconfig
    no me deja iniciar sesion

--- src/main/java/com/example/persistencia/poliglota/config/CorsConfig.java
+++ src/main/java/com/example/persistencia/poliglota/config/CorsConfig.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173") // Frontend Vite
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
