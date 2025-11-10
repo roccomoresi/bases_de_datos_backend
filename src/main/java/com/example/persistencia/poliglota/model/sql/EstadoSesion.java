df732a6a42148db1d4d473af4e251403273bc970
CandeFernandez1 <cfernandez1@uade.edu.ar>
2025-11-10 02:48:56 -0300

    Update Estado Sesion

--- src/main/java/com/example/persistencia/poliglota/model/sql/EstadoSesion.java
+++ src/main/java/com/example/persistencia/poliglota/model/sql/EstadoSesion.java
@@ -1,7 +1,13 @@
 package com.example.persistencia.poliglota.model.sql;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum EstadoSesion {
         
     ACTIVA,
-    INACTIVA
+    INACTIVA;
+        @JsonCreator
+    public static EstadoSesion fromString(String value) {
+        return EstadoSesion.valueOf(value.trim().toUpperCase());
+    }
 }
